16c166f2e80be9c14340442ffa30c78bf093d2ee
mark sauer-utley <mark.utley.713@gmail.com>
2018-04-24 21:58:11 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,52 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  data[season].each do |contestant_hash|
+    if contestant_hash["status"] == "Winner"
+      winner << contestant_hash["name"]
+    end
+  end
+  winner.split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ""
+  data.each do |season, contestant_arr|
+    contestant_arr.each do |contestant_hash|
+      if contestant_hash["occupation"] == occupation
+        name << contestant_hash["name"]
+      end
+    end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+
+  data.each do |season, contestant_arr|
+    contestant_arr.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant_arr|
+    contestant_arr.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        return contestant_hash["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = []
+  data[season].each do |contestant_hash|
+    age_array << contestant_hash["age"].to_f
+  end
+  (age_array.reduce(:+).to_f / age_array.count.to_f).round
 end
